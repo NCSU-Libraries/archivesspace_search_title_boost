9e8b2c6c1858b8e045e345110f9a16c418985303
trevorthornton <trthorn2@ncsu.edu>
2018-02-06 15:09:37 -0500

    add plugin_init

---
+++ backend/plugin_init.rb
@@ -0,0 +1,6 @@
+ArchivesSpaceService.loaded_hook do
+  Solr.add_search_hook do |query|
+    # boost records that exactly match
+    query.add_solr_param('bq', "\"#{query.query_string}\"*")
+  end
+end
