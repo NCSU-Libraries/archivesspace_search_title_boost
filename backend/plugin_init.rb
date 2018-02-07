6c59da31c71e759ec5eb3d673dcb6f522e3049e2
trevorthornton <trthorn2@ncsu.edu>
2018-02-07 15:32:21 -0500

    add pf and ps params

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -2,5 +2,7 @@ ArchivesSpaceService.loaded_hook do
   Solr.add_search_hook do |query|
     # boost records that exactly match
     query.add_solr_param('bq', "title:\"#{@query_string}\"*")
+    query.add_solr_param('pf', 'title^10')
+    query.add_solr_param('ps', 0)
   end
 end
