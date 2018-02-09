6b540745dc17f6ff7b6f1019c520521f510510fb
Trevor Thornton <hellotrevorthornton@gmail.com>
2018-02-09 10:06:01 -0500

    Update plugin_init.rb

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,6 +1,6 @@
 ArchivesSpaceService.loaded_hook do
   Solr.add_search_hook do |query|
-    # boost records that exactly match
+    # boost records that exactly match title field
     query.add_solr_param('bq', "title:\"#{@query_string}\"*")
     query.add_solr_param('pf', 'title^10')
     query.add_solr_param('ps', 0)
